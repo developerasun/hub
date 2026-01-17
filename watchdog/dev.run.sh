0107409a91adcba5b3d714f2ea3a32ad842685b8
developerasun <nellow1102@gmail.com>
2026-01-18 00:38:29 +0900

    clarify pid to clean up

--- watchdog/dev.run.sh
+++ watchdog/dev.run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 echo "🧹 Cleaning up previous processes..."
-trap 'kill $(jobs -p)' EXIT # @dev trap [commands] [signals]
+lsof -ti:8787 | xargs kill -9 2>/dev/null
 
 echo "🚀 Starting Local Worker..."
 pnpm dev:cron &
-sleep 3
+sleep 5
 
 # see here: https://developers.cloudflare.com/workers/runtime-apis/handlers/scheduled/#background
 echo "⏰ Triggering Scheduled Task..."
 curl -sS "http://localhost:8787/__scheduled?cron=*%2F3+*+*+*+*"
+
+echo -e "\n✅ Test finished. Cleaning up..."
+lsof -ti:8787 | xargs kill -9 2>/dev/null
