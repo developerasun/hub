62b960fc57066f3094c3cf7fb865ac330c4b10a0
developerasun <nellow1102@gmail.com>
2026-01-18 20:34:16 +0900

    refactor global cache to rdb for serverless instance change

--- watchdog/src/index.ts
+++ watchdog/src/index.ts
@@ -2,22 +2,26 @@ import { Hono } from "hono";
 import { env } from "cloudflare:workers";
 import { payload } from './template'
 import { getConnection, HUMAN_BOOLEAN, tHealthiness } from "./database/schema";
-import { eq } from "drizzle-orm";
+import { lt } from "drizzle-orm";
 
 const devRun = env.PRODUCTION === "false"
 const webhook = env.WEBHOOK_ENDPOINT
 const proxy = env.PROXY_ENDPOINT
-const healtiness = new Map<string, boolean>([["status", false]])
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app.get("/health", (c) => {
   return c.text("ok");
 });
 
-const skippable = () => healtiness.get("status")!
-const recover = async (time: string) => {
-  const current = skippable()
-  if (current == false) {
+const skippable = async () => {
+  const { connection } = await getConnection(env.DB)
+  const { status } = (await connection.select({ status: tHealthiness.status }).from(tHealthiness).limit(1).get())!
+  return status
+}
+const recover= async (time: string) => {
+  const { connection } = await getConnection(env.DB)
+  const { status } = (await connection.select({ status: tHealthiness.status }).from(tHealthiness).limit(1).get())!
+  if (status == HUMAN_BOOLEAN.false) {
     await fetch(webhook, {
       method: "POST",
       headers: {
@@ -25,14 +29,16 @@ const recover = async (time: string) => {
       },
       body: JSON.stringify(payload.restore(time))
     })
+    await connection.update(tHealthiness).set({ status: HUMAN_BOOLEAN.true }).where(lt(tHealthiness.id, 10))
   }
-
-  healtiness.set("status", true)
 }
-const reset = () => healtiness.set("status", false)
+const reset = async () => {
+  const { connection } = await getConnection(env.DB)
+  await connection.update(tHealthiness).set({ status: HUMAN_BOOLEAN.false }).where(lt(tHealthiness.id, 10))
+}
 
 const alert = async (time: string) => {
-  if (skippable()) {
+  if (await skippable()) {
     return
   }
 
@@ -87,7 +93,7 @@ export default {
         if (response.status == 200) {
           await recover(time)
         } else {
-          reset()
+          await reset()
         }
         // @dev alert an fire and ignore
         await alert(time)
